3271761789391781f1aeca7844cea2245ec8574c
harsh <harshit_agrawal36@yahoo.com>
2017-12-30 15:15:21 -0800

    update test inputed

---
+++ test/update_test.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating Records ', () => {
+  let joe;
+
+  beforeEach((done) =>{
+    joe = new User({name: 'joe', email: 'joe@gmail.com' , age: 19})
+    joe.save().then(()=> done());
+  });
+
+  function assertName(operation, done) {
+    operation
+    .then(() => User.find({}))
+    .then((users)=> {
+      assert(users.length === 1);
+      assert(users[0].name === 'Alex');
+      done();
+    });
+}
+
+  it('Instance set n save ', (done) => {
+    joe.set('name' , 'Alex');
+    assertName(joe.save(), done);
+  });
+
+  it('Model Instance Update', (done) =>{
+    assertName(joe.update({name: 'Alex'}), done);
+  })
+
+  it('class Update' , (done) => {
+    assertName(User.update({name:'joe'}, {name:'Alex'}),done);
+  });
+
+  it('class one Update' , (done) => {
+    assertName(User.findOneAndUpdate({name: 'joe'}, { name: 'Alex'}),done);
+  });
+
+  it('class Update by Id' , (done) => {
+    assertName(User.findByIdAndUpdate(joe._id,{name:'Alex'}),done);
+
+  });
+});
